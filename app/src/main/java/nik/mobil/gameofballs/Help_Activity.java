c08a4c12178e5fff90ebec5b2f6a21e0c5996639
Patrik_Páldi <b3stsnip3r@gmail.com>
2015-05-01 16:56:13 +0200

    Help

--- app/src/main/java/nik/mobil/gameofballs/Help_Activity.java
+++ app/src/main/java/nik/mobil/gameofballs/Help_Activity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class Help_Activity extends ActionBarActivity {
@@ -12,6 +13,11 @@ public class Help_Activity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_);
+    }
+
+    public void BackToMain(View view)
+    {
+        finish();
     }
 
 
