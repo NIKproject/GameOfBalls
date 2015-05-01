1563b160661d17edc87c17e0ee2cdb49c6d52e68
Patrik_Páldi <b3stsnip3r@gmail.com>
2015-05-01 14:46:29 +0200

    Exit :)

--- app/src/main/java/nik/mobil/gameofballs/Main.java
+++ app/src/main/java/nik/mobil/gameofballs/Main.java
@@ -1,9 +1,12 @@
 package nik.mobil.gameofballs;
 
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Main extends ActionBarActivity {
@@ -12,6 +15,17 @@ public class Main extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button exitbut = (Button) findViewById(R.id.exit_button);
+        exitbut.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                exit();
+            }
+        });
+    }
+
+    private void exit() {
+        finish();
     }
 
 
