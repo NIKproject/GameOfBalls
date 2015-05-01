90492b8fe045c51cb8dc14aa5b7edcca195402fc
Patrik_Páldi <b3stsnip3r@gmail.com>
2015-05-01 17:32:50 +0200

    Help

--- app/src/main/java/nik/mobil/gameofballs/Score_Activity.java
+++ app/src/main/java/nik/mobil/gameofballs/Score_Activity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class Score_Activity extends ActionBarActivity {
@@ -12,6 +13,10 @@ public class Score_Activity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_);
+    }
+    public void BackToMain(View view)
+    {
+        finish();
     }
 
 
